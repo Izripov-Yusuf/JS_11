ef667c28bf6fe3837f3309a9056a846695e5eb4f
Izripov-Yusuf <izripov1998@gmail.com>
2020-01-27 20:59:43 +0300

    Внёс правки

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 'use strict';
 
-let money = 30000, income = '15000', addExpenses = 'Аренда, Еда, Интернет, Коммуналка, Транспорт',
-  deposit = false, mission = 100000, period = 3;
+let money = 30000, income = '15000', deposit = false, mission = 100000, period = 3;
 
 console.log(typeof money, typeof income, typeof deposit);
 
-console.log(addExpenses.length);
-
 console.log("Период равен " + period + " месяца. " + "Цель заработать " + mission + " рублей");
-
-console.log(addExpenses.toLowerCase().split(','));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
 
 money = prompt('Ваш месячный доход ?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 deposit = confirm('Есть ли у вас депозит в банке?');
 
 let expenses1 = prompt('Введите обязательную статью расходов?'),
-expenses2 = prompt('Введите обязательную статью расходов?'),
 amount1 = +prompt('Во сколько это обойдется?'),
+expenses2 = prompt('Введите обязательную статью расходов?'),
 amount2 = +prompt('Во сколько это обойдется?');
 
 let budgetMonth = money - (amount1 + amount2);
