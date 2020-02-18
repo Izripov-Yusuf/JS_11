7e758ac36a1e293849263bcaec92c97143e839a2
Izripov-Yusuf <izripov1998@gmail.com>
2020-02-18 19:58:10 +0300

    Удалил цикл, теперь всё работает нормально

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  let thisYear = new Date().getFullYear(),
-      thisMonth = new Date().getMonth(),
-      thisDay = new Date().getDate() + 1;
-
   // Таймер
   function countTimer(deadline) {
     let timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
 
     function getTimeRemaining() {
       let dateStop = new Date(deadline).getTime(),
@@ -18,19 +14,14 @@ window.addEventListener('DOMContentLoaded', function () {
         seconds = Math.floor(timeRemaining % 60),
         minutes = Math.floor((timeRemaining / 60) % 60),
         hours = Math.floor(timeRemaining / 60 / 60);
-        // day = Math.floor(timeRemaining / 60 / 60 / 24)
-      return {timeRemaining, hours, minutes, seconds};
+      // day = Math.floor(timeRemaining / 60 / 60 / 24)
+      return {
+        timeRemaining,
+        hours,
+        minutes,
+        seconds
+      };
     }
-
-    /* function checkTime(i) {
-      if (i < 10 && i > 0) {
-        i = "0" + i;
-      }
-      if (i < 0) {
-        i = "0" + 0;
-      }
-      return i;
-    } */
 
     function checkTime(i) {
       if (i < 10 && i >= 0) {
@@ -51,11 +42,7 @@ window.addEventListener('DOMContentLoaded', function () {
       }
 
     }
-    setInterval(updateClock, 1000);
     updateClock();
   }
-  countTimer(new Date(thisYear, thisMonth, thisDay));
-  if (countTimer(new Date(thisYear, thisMonth, thisDay)) === 0) {
-    countTimer(new Date(thisYear, thisMonth, thisDay));
-  }
+  setInterval(countTimer, 1000, '1 february 2020');
 });
