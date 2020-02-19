97aed6490aab2ad1fdc78664681da0ff46417cef
Izripov-Yusuf <izripov1998@gmail.com>
2020-02-19 22:38:44 +0300

    18 урок, обязательное

--- script.js
+++ script.js
@@ -41,4 +41,71 @@ window.addEventListener('DOMContentLoaded', function () {
   }
   setInterval(countTimer, 1000, '19 february 2020');
   countTimer('19 february 2020');
+
+  // Меню
+  const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+      const handlerMenu = () => {
+        menu.classList.toggle('active-menu');
+      };
+
+    btnMenu.addEventListener('click', handlerMenu);
+
+    closeBtn.addEventListener('click', handlerMenu);
+    
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+  };
+  toggleMenu();
+
+  //popup
+  /* const handlerMenu = () => {
+      if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+        menu.style.transform = `translate(0)`;
+      } else {
+        menu.style.transform = `translate(-100%)`;
+      }
+    }; */
+  const togglePopup = () => {
+    let popupInterval,
+        count = 0,
+        userWidth = window.innerWidth;
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close'),
+          popupBlock = document.querySelector('.popup-content');
+
+    const popupAnimation = () => {
+      popupInterval = requestAnimationFrame(popupAnimation);
+      count++;
+      if (count <= 38) {
+        popupBlock.style.left = count + '%';
+      } else {
+        cancelAnimationFrame(popupInterval);
+        count = 0;
+      }
+    };
+    
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';
+        popupBlock.style.left = '-20%';
+        if (!popup.style.display || popup.style.display === `block` && userWidth >= 768) {
+          popupInterval = requestAnimationFrame(popupAnimation);
+        } else if (userWidth < 768) {
+          cancelAnimationFrame(popupInterval);
+          popupBlock.style.left = '30%';
+        }
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+  };
+  togglePopup();
 });
