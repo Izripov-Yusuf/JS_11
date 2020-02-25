59b671255e32dcf3570152f0fc04a70e5c99f3fb
Izripov-Yusuf <izripov1998@gmail.com>
2020-02-25 17:43:30 +0300

    Обязательное и усложнённое

--- script.js
+++ script.js
@@ -308,21 +308,19 @@ window.addEventListener('DOMContentLoaded', function () {
   const changeImg = () => {
     const commandWrap = document.querySelector('div#command>div.container>div.row');
 
+    let stockSrc;
+
     commandWrap.addEventListener('mouseover', (event) => {
       let target = event.target;
       if (target.matches('img')) {
-        let stockSrc = target.src;
+        stockSrc = target.src;
         target.src = target.dataset.img;
-        target.addEventListener('mouseout', () => {
-          target.src = stockSrc;
-        });
       }
     });
 
     commandWrap.addEventListener('mouseout', (event) => {
       let target = event.target;
       if (target.matches('img')) {
-        let stockSrc = target.src;
         target.src = stockSrc;
       }
     });
@@ -334,8 +332,63 @@ window.addEventListener('DOMContentLoaded', function () {
     const calcBlock = document.querySelector('.calc-block');
     calcBlock.addEventListener('input', (event) => {
       let target = event.target;
-      target.value = target.value.replace(/[^0-9]/, '');
+      if (target.matches('input')) {
+        target.value = target.value.replace(/[^0-9]/, '');
+      }
     });
   };
   calcValidation();
+
+  // Калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcDay = document.querySelector('.calc-day'),
+          calcCount = document.querySelector('.calc-count'),
+          totalValue = document.getElementById('total');
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+
+      const countSum = () => {
+        let total = 0,
+            countValue = 1,
+            dayValue = 1,
+            count = 0,
+            totalId;
+        const typeValue = calcType.options[calcType.selectedIndex].value,
+              squareValue = +calcSquare.value;
+
+        if (calcCount.value > 1) {
+          countValue += (calcCount.value - 1) / 10;
+        }
+
+        if (calcDay.value && calcDay.value < 5) {
+          dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+          dayValue *= 1.5;
+        }
+
+        if (typeValue && squareValue) {
+          total = price * typeValue * squareValue * countValue * dayValue;
+        }
+
+        totalValue.textContent = total;
+        const foo = () => {
+          totalValue.textContent = count;
+          if (count < total) {
+            count++;
+          } else {
+            clearInterval(totalId);
+          }
+        };
+        totalId = setInterval(foo, 0.1);
+      };
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+  calc(100);
 });
