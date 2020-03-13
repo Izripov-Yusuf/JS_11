a0192a509746a1e16e01d32fed371a3e20348fc1
Izripov-Yusuf <izripov1998@gmail.com>
2020-03-13 20:55:22 +0300

    Реализовал открытие меню по клику на бургер и закрытие при клике на крестик или пункт меню

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,5 +1,39 @@
-const test = () => {
-  console.log('Проверка работы модулей вебпака');
+const burgerMenu = () => {
+  const burger = document.getElementById('burger'),
+        burgerMenu = document.querySelector('.popup-menu'),
+        menuLinks = burgerMenu.querySelectorAll('a'),
+        closeMenu = document.getElementById('close-menu'),
+        body = document.querySelector('body');
+
+  const handlerMenu = () => {
+    burgerMenu.classList.toggle('popup-menu-active');
+  };
+
+  const scrollToBlock = (index) => {
+    for (let i = 0; i < menuLinks.length; i++) {
+      if (index === i) {
+        handlerMenu();
+      }
+    }
+  };
+
+  body.addEventListener('click', (event) => {
+    let target = event.target,
+      parent = target.parentNode;
+
+    if (target === burger) {
+      handlerMenu();
+    }
+    if (target === closeMenu) {
+      handlerMenu();
+    } else if (parent.tagName === 'LI') {
+      menuLinks.forEach((item, i) => {
+        if (item === target) {
+          scrollToBlock(i);
+        }
+      });
+    }
+  });
 };
 
-export default test;
+export default burgerMenu;
