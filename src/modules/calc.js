9712158329a562e3b2aceec206bdc98111a82d1e
Izripov-Yusuf <izripov1998@gmail.com>
2020-03-15 23:39:34 +0300

    Исправил ошибку с промокодом

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -9,7 +9,7 @@ const calc = () => {
 
   if (time) {
     const sale = () => {
-      promocode.addEventListener('input', () => {
+      promocode.addEventListener('change', () => {
         if (promocode.value === 'ТЕЛО2020') {
           priceTotal.textContent = priceTotal.textContent - Math.ceil(((priceTotal.textContent / 100) * 30));
         }
