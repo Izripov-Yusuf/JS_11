4bf182540b7f0568e01bf3ce54f384afdfdee5d1
Izripov-Yusuf <izripov1998@gmail.com>
2020-03-16 16:15:21 +0300

    Мучения с промокодом

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -12,8 +12,6 @@ const calc = () => {
       promocode.addEventListener('input', () => {
         if (promocode.value === 'ТЕЛО2020') {
           priceTotal.textContent = priceTotal.textContent - Math.ceil(((priceTotal.textContent / 100) * 30));
-        } else if (promocode.value !== 'ТЕЛО2020') {
-          check();
         }
       });
     };
